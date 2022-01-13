5df7971389fa92228779e714cfe5b4013bcfe363
june0039 <74568248+june0039@users.noreply.github.com>
2022-01-13 13:36:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,26 @@ console.log("Hello world")
 Multi-line comment
 */
 
-// Vatiables
-var sebastian = 6;
+// Variables
+
+// var can be changed later, will be hoised
+var sebastian = 6; //numbers
+
 sebastian = 3;
-const kevin = "kevin";
+
+// constant can't be changed.
+const kevin = "kevin"; //strings
+
+//let can be changed, just use this
+let julie  = 2.9; // also a number
+julie = 3;
+
+let kai = "Kai";
+
+//assignment, manipulation
+julie = 3; // assignment
+julie = julie + 2; // addition
+julie = julie - 4; // subtraction
+julie = julie + 2;
+julie = julie + 2;
+// Variables
